684660c08c853d4012db224f4475e105993a673d
Hilke Ros <hilke@mmmotion.com>
2012-09-07 00:29:46 +0200

    index and edit pages for news

--- spec/requests/news_pages_spec.rb
+++ spec/requests/news_pages_spec.rb
@@ -35,4 +35,52 @@ describe "NewsPages" do
       end
     end
   end
+  
+  describe "edit page" do
+    let(:user) { FactoryGirl.create(:post) }
+    before { visit edit_admin_post_path }
+    
+    describe "page" do
+      it { should have_selector('h1', text: "Bewerk nieuwsbericht") }
+      it { should have_selector('title', text: "Bewerk nieuwsbericht") }
+    end
+    
+    describe "with invalid information" do
+      before { click_button "Bewaar" }
+      
+      it { should have_content('fout(en)') }
+    end
+    
+    describe "with valid information" do
+      let(:new_title)   { "New Title" }
+      let(:new_body)    { "New body" }
+      before do
+        fill_in "Titel", with: new_title
+        fill_in "Bericht", with: new_body
+        click_button "Bewaren"
+      end
+      
+      it { should have_selector('title', text: new_title) }
+      it { should have_selector('div.alert.alert-success') }
+      specify { post.reload.title.should == new_title }
+      specify { post.reload.body.should == new_body }
+    end
+  end
+  
+  describe "index" do
+    before do
+      FactoryGirl.create(:post, title: "Titel 1", body: "Dit is een tekst")
+      FactoryGirl.create(:post, title: "Titel 2", body: "Dit is nog een tekst")
+      visit admin_posts_path
+    end
+    
+    it { should have_selector('title', text: 'Nieuwsberichten') }
+    it { should have_selector('h1', text: 'Nieuwsberichten') }
+    
+    it "should list each post" do
+      Post.all.each do |post|
+        page.should have_selector('a', text: post.title)
+      end
+    end
+  end
 end
